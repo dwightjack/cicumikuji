2f3a373b1f54028dd71c5dde32b364256f741fdf
dwightjack <marco.solazzi@gmail.com>
2021-03-10 10:51:18 +0900

    loader animation

--- public/components/Loader/Loader.styles.ts
+++ public/components/Loader/Loader.styles.ts
@@ -2,10 +2,10 @@ import { styled, keyframes } from 'goober';
 
 const bounce = keyframes`
   0%   { transform: scale(1,1)      translateY(0); }
-  10%  { transform: scale(1.1,.9)   translateY(0); }
+  5%  { transform: scale(1.1,.9)   translateY(0); }
   30%  { transform: scale(.9,1.1)   translateY(-100px); }
-  50%  { transform: scale(1.05,.95) translateY(0); }
-  57%  { transform: scale(1,1)      translateY(-7px); }
+  40%  { transform: scale(1,1)      translateY(-7px); }
+  50%  { transform: scale(1.05,.95) translateY(7px); }
   64%  { transform: scale(1,1)      translateY(0); }
   100% { transform: scale(1,1)      translateY(0); }
 `;
@@ -29,7 +29,7 @@ export const Svg = styled('svg')`
   height: 50%;
   object-fit: contain;
   animation-name: ${bounce};
-  animation-timing-function: cubic-bezier(0.28, 0.84, 0.42, 1);
+  animation-timing-function: cubic-bezier(0.28, 0.84, 0.23, 0.99);
   animation-duration: 1.7s;
   animation-iteration-count: infinite;
 `;
