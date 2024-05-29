6fa61aba1f77692700edf5db179611786186a9c4
dwightjack <marco.solazzi@gmail.com>
2024-05-29 16:55:59 +0900

    fix issues

--- packages/scripts/refresh.mjs
+++ packages/scripts/refresh.mjs
@@ -66,64 +66,68 @@ async function initialize() {
 
   console.log(`Found ${entries.length} posts.`);
 
-  for (const entry of entries) {
-    const timestamp =
-      (entry.creation_timestamp || entry.media.at(0)?.creation_timestamp) *
-      1000;
+  try {
+    for (const entry of entries) {
+      const timestamp =
+        (entry.creation_timestamp || entry.media.at(0)?.creation_timestamp) *
+        1000;
 
-    if (timeStamps.includes(timestamp)) {
-      console.warn(
-        `Matched a post already saved (datetime: ${new Date(
-          timestamp,
-        ).toISOString()})`,
+      if (timeStamps.includes(timestamp)) {
+        console.warn(
+          `Matched a post already saved (datetime: ${new Date(
+            timestamp,
+          ).toISOString()})`,
+        );
+        continue;
+      }
+      /**
+       * Extract the title:
+       * - for images, it's in the first media object: entry.media[0].title
+       * - for videos, it's in the root object: entry.title
+       */
+      // here, upload the image
+      const title =
+        entry.title || entry.media.map(({ title }) => title).filter(Boolean)[0];
+
+      // only use the first photo from the set
+      const src = entry.media
+        .map(({ uri }) => uri)
+        .filter((uri) => uri?.endsWith('.jpg'))[0];
+
+      if (!src) {
+        continue;
+      }
+
+      const imageSrcPath = join(DATA_DIR, src);
+      if (!existsSync(imageSrcPath)) {
+        console.warn(`File ${imageSrcPath} not found`);
+        continue;
+      }
+      const ext = extname(imageSrcPath);
+      const [uploadFile] = await bucket.upload(imageSrcPath, {
+        destination: `post-${timestamp}${ext}`,
+        public: true,
+      });
+      const docRef = postRefs.doc();
+
+      const newPost = {
+        src: uploadFile.publicUrl(),
+        id: docRef.id,
+        videoUrl: null,
+        caption: Buffer.from(title, 'latin1').toString('utf-8'),
+        datetime: new Date(timestamp).toISOString(),
+        timestamp,
+        local: true,
+      };
+
+      batch.set(docRef, newPost);
+      console.log(
+        `Pushed post: ${docRef.id} - ${new Date(timestamp).toISOString()}`,
       );
-      continue;
     }
-    /**
-     * Extract the title:
-     * - for images, it's in the first media object: entry.media[0].title
-     * - for videos, it's in the root object: entry.title
-     */
-    // here, upload the image
-    const title =
-      entry.title || entry.media.map(({ title }) => title).filter(Boolean)[0];
-
-    // only use the first photo from the set
-    const src = entry.media
-      .map(({ uri }) => uri)
-      .filter((uri) => uri?.endsWith('.jpg'))[0];
-
-    if (!src) {
-      continue;
-    }
-
-    const imageSrcPath = join(DATA_DIR, src);
-    if (!existsSync(imageSrcPath)) {
-      console.warn(`File ${imageSrcPath} not found`);
-      continue;
-    }
-    const ext = extname(imageSrcPath);
-    const [uploadFile] = await bucket.upload(imageSrcPath, {
-      destination: `post-${timestamp}${ext}`,
-      public: true,
-    });
-    const docRef = postRefs.doc();
-
-    const newPost = {
-      src: uploadFile.publicUrl(),
-      id: docRef.id,
-      videoUrl: null,
-      caption: Buffer.from(title, 'latin1').toString('utf-8'),
-      datetime: new Date(timestamp).toISOString(),
-      timestamp,
-      local: true,
-    };
-
-    batch.set(docRef, newPost);
-    console.log(
-      `Pushed post: ${docRef.id} - ${new Date(timestamp).toISOString()}`,
-    );
+  } catch (e) {
+    console.warn(e);
+  } finally {
+    batch.commit();
   }
-
-  batch.commit();
 })();
